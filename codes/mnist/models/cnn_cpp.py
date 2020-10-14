4a0eb69b79184c723c71f9c45e3951a3154d98a6
hyuck2 <hyuck2@outlook.kr>
2020-10-14 13:54:40 +0900

    cnn_cpp modified

--- codes/mnist/models/cnn_cpp.py
+++ codes/mnist/models/cnn_cpp.py
@@ -5,47 +5,26 @@ import cnn_cpp
 class cnn(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.structure = []
         self.conv2d_00 = torch.nn.Conv2d(1, 32, (3, 3), padding=1) # 32, 32
         self.relu_00 = torch.nn.ReLU()
-        self.structure.append(0)
-        self.structure.append(1)
         self.conv2d_01 = torch.nn.Conv2d(32, 32, (3, 3), stride=2, padding=1) # 32, 32
         self.relu_01 = torch.nn.ReLU()
-        self.structure.append(2)
-        self.structure.append(1)
         self.conv2d_02 = torch.nn.Conv2d(32, 64, (3, 3), padding=1) # 64, 64
         self.relu_02 = torch.nn.ReLU()
-        self.structure.append(0)
-        self.structure.append(1)
         self.conv2d_03 = torch.nn.Conv2d(64, 64, (3, 3), stride=2, padding=1) # 64, 64
         self.relu_03 = torch.nn.ReLU()
-        self.structure.append(2)
-        self.structure.append(1)
         self.conv2d_04 = torch.nn.Conv2d(64, 128, (3, 3), padding=1) # 128, 128
         self.relu_04 = torch.nn.ReLU()
-        self.structure.append(0)
-        self.structure.append(1)
         self.conv2d_05 = torch.nn.Conv2d(128, 128, (3, 3), stride=2, padding=1) # 128, 128
         self.relu_05 = torch.nn.ReLU()
-        self.structure.append(2)
-        self.structure.append(1)
         self.conv2d_06 = torch.nn.Conv2d(128, 256, (3, 3), padding=1) # 256, 256
         self.relu_06 = torch.nn.ReLU()
-        self.structure.append(0)
-        self.structure.append(1)
         self.conv2d_07 = torch.nn.Conv2d(256, 256, (3, 3), stride=2, padding=1) # 256, 256
         self.relu_07 = torch.nn.ReLU()
-        self.structure.append(2)
-        self.structure.append(1)
         self.conv2d_08 = torch.nn.Conv2d(256, 512, (3, 3), padding=1) # 512, 512
         self.relu_08 = torch.nn.ReLU()
-        self.structure.append(0)
-        self.structure.append(1)
         self.conv2d_09 = torch.nn.Conv2d(512, 512, (3, 3), stride=2, padding=1) # 512, 512
         self.relu_09 = torch.nn.ReLU()
-        self.structure.append(2)
-        self.structure.append(1)
         self.linear_00 = torch.nn.Linear(512, 50) # weight 50, bias 50
         self.relu_10 = torch.nn.ReLU()
         self.linear_01 = torch.nn.Linear(50, 10) # weight 10, bias 10
