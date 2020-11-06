50aa67e0558b466d45b63c8de066e685fc03bf86
hyuck2 <hyuck2@outlook.kr>
2020-11-06 14:02:12 +0900

    linear cuda

--- codes/mnist/extension/fc_cpp.cpp
+++ codes/mnist/extension/fc_cpp.cpp
@@ -23,25 +23,3 @@ torch::Tensor forward(torch::Tensor input, std::vector<torch::Tensor> parameter)
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("forward", &forward, "Fully connected forward (CPP)");
 }
-
-/*
-linear
-found on torch/csrc/api/include/torch/nn/functional/linear.h
-inline Tensor linear(const Tensor& input, const Tensor& weight,
-                     const Tensor& bias = {}) {
-  if (input.dim() == 2 && bias.defined()) {
-    // fused op is marginally faster
-    return torch::addmm(bias, input, weight.t());
-  } else {
-    auto output = input.matmul(weight.t());
-    if (bias.defined()) {
-        output += bias;
-    }
-    return output;
-  }
-}
-
-relu
-found on torch/csrc/api/include/torch/nn/functional/activation.h
-
-*/
--- codes/mnist/extension/fc_cuda.cu
+++ codes/mnist/extension/fc_cuda.cu
@@ -3,7 +3,13 @@
 #include <cuda_runtime.h>
 #include <vector>
 
-__global__ void forward_kernel(){
+template <typename scalar_t>
+__global__ void forward_kernel(
+  torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> input,
+  torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> weight,
+  torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> bias,
+  torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> output
+){
   /*
   input
   weight
@@ -32,11 +38,48 @@ __global__ void forward_kernel(){
 
   
   */
+  int batch = blockIdx.x;
+  int img_ = threadIdx.x;
+  __shared__ float image[blockDim.x]; // declare s_mem size with kernel launch
+  //input size --> blockdim?
+  image[threadIdx.x] = input[threadIdx.x]*weight[blockIdx.x] + bias[blockIdx.x]; // fused operation should be used here
+
+  // sequential addition
+  __syncthreads();
+  if (threadIdx.x==0){
+    for(int tid=1;tid<blockDim.x;tid++){
+      image[threadIdx.x] += image[threadIdx.x+tid];
+    }
+    
+    output[blockIdx.x] = image[threadIdx.x];
+  }
+  // reduction
+  /*
+  __syncthreads();
+  for (int step=1; step<input_size;step*=2){
+    if ((threadIDx.x%(2*step)==0)&&(threadIdx.x<input_size)){
+      image[threadIdx.x] += image[threadIdx.x+step];
+    }
+    __syncthreads();
+  }
+  if (threadIdx.x == 0){
+    // ReLU
+    if (image[threadIdx.x]<0){
+      output[blockIdx.x] = 0.0;
+    }
+    else{
+      output[blockIdx.x] = image[threadIdx.x];
+    }
+  }
+  */
 }
 
 torch::Tensor forward(torch::Tensor input, std::vector<torch::Tensor> parameter){
-    torch::Tensor output;
-    
+    AT_DISPATCH_FLOATING_TYPES(.type(), "cuda_forward", ([&] {
+      forward_kernel<scalar_t><<<64,784>>>(
+
+      );
+    }));
     return output;
 }
 
